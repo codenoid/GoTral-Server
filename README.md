5521461c56a3fa3e499b75fa9e314cfb1a2b718b
codenoid <14269809+codenoid@users.noreply.github.com>
2019-10-24 01:14:35 +0700

    WIP:test[1]

---
+++ README.md
@@ -0,0 +1,13 @@
+# GoTral Server
+
+server for [gotral library](https://github.com/codenoid/GoTral)
+
+## Usage
+
+```
+$ git clone  https://github.com/codenoid/GoTral-Server.git
+$ cd GoTral-Server
+// edit some config / do what you want
+$ go run server.go
+starting to listen on :6969
+```
---
+++ server.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+	"os"
+	"io/ioutil"
+)
+
+var (
+	path = "./config/" // remember to end with /
+	BasicAuth = false
+	validUsername = "guest"
+	validPassword = "guest"
+)
+
+
+// fileExists checks if a file exists and is not a directory before we
+// try using it to prevent further errors.
+func fileExists(filename string) bool {
+    info, err := os.Stat(filename)
+    if os.IsNotExist(err) {
+        return false
+    }
+    return !info.IsDir()
+}
+
+// getConfig : handler for getting config data
+func getConfig(w http.ResponseWriter, r *http.Request) {
+
+	if BasicAuth == true {
+		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+
+		username, password, authOK := r.BasicAuth()
+		if authOK == false {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		if username != validUsername || password != validPassword {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+	}
+
+	if r.Method == "GET" {
+		// get url query
+		keys := r.URL.Query()
+
+		// filename of project id
+		ConfigFile := keys.Get("id")
+
+		// path already end with slash
+		if fileExists(path + ConfigFile) {
+		    b, err := ioutil.ReadFile(path + ConfigFile)
+		    if err != nil {
+		        http.Error(w, "Failed to read file", 500)
+		        return
+		    }
+
+		    // return json content from file as string
+		    fmt.Fprintf(w, string(b))
+		    return
+		} else {
+		    http.Error(w, "File doesn't exist!", 500)
+		    return
+		}
+	}
+    fmt.Fprintf(w, "only get my dudeeeeeeeeeeee")
+}
+
+func main() {
+    http.HandleFunc("/config", getConfig)
+    fmt.Println("starting to listen on :6969")
+    log.Fatal(http.ListenAndServe(":6969", nil))
+}
