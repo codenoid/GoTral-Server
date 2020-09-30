36df0347e9b982ee6f8fc3901580ef3a10063996
kai <14269809+codenoid@users.noreply.github.com>
2020-09-30 15:30:57 +0700

    separate util functionality, allow bind to setup via args

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/codenoid/GoTral
+
+go 1.15
+
+require github.com/mervick/aes-everywhere/go/aes256 v0.0.0-20200907200845-eff87648c75f
--- server.go
+++ server.go
@@ -1,32 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-)
 
-import "github.com/mervick/aes-everywhere/go/aes256"
+	"github.com/mervick/aes-everywhere/go/aes256"
+)
 
 var (
-	path            = "./config/" // remember to end with /
+	configPath      string
 	encryptPassword = "somehardpw"
-	BasicAuth       = false
-	validUsername   = "guest"
-	validPassword   = "guest"
-)
 
-// fileExists checks if a file exists and is not a directory before we
-// try using it to prevent further errors.
-func fileExists(filename string) bool {
-	info, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return !info.IsDir()
-}
+	// BasicAuth functionality
+	BasicAuth  bool
+	BAUsername string
+	BAPassword string
+
+	// server utility
+	bind string
+)
 
 // getConfig : handler for getting config data
 func getConfig(w http.ResponseWriter, r *http.Request) {
@@ -40,42 +35,41 @@ func getConfig(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		if username != validUsername || password != validPassword {
+		if username != BAUsername && password != BAPassword {
 			http.Error(w, "Not authorized", 401)
 			return
 		}
 	}
 
-	if r.Method == "GET" {
-		// get url query
-		keys := r.URL.Query()
+	// filename of project id
+	configFile := r.URL.Query().Get("id")
 
-		// filename of project id
-		ConfigFile := keys.Get("id")
-
-		// path already end with slash
-		if fileExists(path + ConfigFile) {
-			b, err := ioutil.ReadFile(path + ConfigFile)
-			if err != nil {
-				http.Error(w, "Failed to read file", 500)
-				return
-			}
-
-			encrypted := aes256.Encrypt(string(b), encryptPassword)
-
-			// return json content from file as string
-			w.Write([]byte(encrypted))
-			return
-		} else {
-			http.Error(w, "File doesn't exist!", 500)
+	// path already end with slash
+	path := configPath + "/" + configFile
+	if fileExists(path) {
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			http.Error(w, "Failed to read file", 500)
 			return
 		}
+
+		encrypted := aes256.Encrypt(string(b), encryptPassword)
+
+		// return json content from file as string
+		w.Write([]byte(encrypted))
+		return
 	}
-	fmt.Fprintf(w, "only get my dudeeeeeeeeeeee")
+
+	http.Error(w, "File doesn't exist!", 404)
 }
 
 func main() {
+
+	flag.StringVar(&bind, "bind", ":8080", "-bind 0.0.0.0:3000")
+	flag.Parse()
+
 	http.HandleFunc("/config", getConfig)
-	fmt.Println("starting to listen on :6969")
-	log.Fatal(http.ListenAndServe(":6969", nil))
+
+	fmt.Println("starting to listen on", bind)
+	log.Fatal(http.ListenAndServe(bind, nil))
 }
